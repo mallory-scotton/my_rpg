599a0aa0dd3c36471ac65717d795d8e42568f226
Mallow <mallory.scotton@epitech.eu>
2024-02-21 19:54:15 +0100

    ✨ Adding dependencies for the project

---
+++ include/rpg/dependencies.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** ak-axolotl
+** File description:
+** dependencies
+*/
+
+#ifndef DEPENDENCIES_H_
+    #define DEPENDENCIES_H_
+
+///////////////////////////////////////////////////////////////////////////////
+// Headers
+///////////////////////////////////////////////////////////////////////////////
+    #include <SFML/Audio.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/Network.h>
+    #include <SFML/Window.h>
+    #include <SFML/System.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <dirent.h>
+    #include <sys/stat.h>
+    #include <limits.h>
+    #include <math.h>
+    #include <string.h>
+    #include <malloc.h>
+
+#endif /* !DEPENDENCIES_H_ */
